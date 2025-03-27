203d894381c31ff401f38046081c0f44a7c64ffc
Zack Armand <113147512+ZacharyArmandNEU@users.noreply.github.com>
2025-03-27 13:48:51 -0400

    Updating .sh to do weather data

--- iTransformer.sh
+++ iTransformer.sh
@@ -7,23 +7,21 @@ conda activate ./venv
 
 python -u run.py \
   --is_training 1 \
-  --root_path ./dataset/traffic/ \
-  --data_path traffic.csv \
-  --model_id traffic_96_96 \
+  --root_path ./dataset/weather/ \
+  --data_path weather.csv \
+  --model_id weather_96_96 \
   --model $model_name \
   --data custom \
   --features M \
   --seq_len 96 \
   --pred_len 96 \
-  --e_layers 4 \
-  --enc_in 862 \
-  --dec_in 862 \
-  --c_out 862 \
+  --e_layers 3 \
+  --enc_in 21 \
+  --dec_in 21 \
+  --c_out 21 \
   --des 'Exp' \
   --d_model 512\
-  --d_ff 512 \
-  --batch_size 16 \
-  --learning_rate 0.001 \
+  --d_ff 512\
   --itr 1 \
   --use_gpu True \
   --num_workers 0 \
